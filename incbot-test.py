e31ea6395befeb20a96a0eb2c16edad81ba471f2
ishhadev <150347755+ishhadev@users.noreply.github.com>
2024-04-29 23:31:33 +0530

    Add files via upload

--- incbot-test.py
+++ incbot-test.py
@@ -1,7 +1,6 @@
 import os
-import streamlit as st
-import google.generativeai as genai
 import PyPDF2
+import dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
@@ -9,6 +8,8 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+import streamlit as st
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
